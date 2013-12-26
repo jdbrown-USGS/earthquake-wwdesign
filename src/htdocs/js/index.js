b6248b31fc826d24667104f6d7e38307ff52e415
Jonathan Brown <jdbrown@usgs.gov>
2013-12-26 15:04:37 -0700

    included tablist from templet

--- src/htdocs/js/index.js
+++ src/htdocs/js/index.js
@@ -3,7 +3,8 @@ require.config({
 	paths: {
 		'leaflet': '/leaflet/dist/leaflet-src',
 		'mvc': '/hazdev-webutils/src/mvc',
-		'util': '/hazdev-webutils/src/util'
+		'util': '/hazdev-webutils/src/util',
+		'tablist': '/hazdev-tablist/src/tablist'
 	},
 	shim: {
 		leaflet: {
